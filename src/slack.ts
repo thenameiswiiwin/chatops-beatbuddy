c34c2962cf0a3c338c7540c718a8f1c72dc43711
thenameiswiiwin <huyn.nguyen95@gmail.com>
2023-08-10 11:57:10 -0400

    processing of Slack slash commands using "@netlify/functions". It refines responses for the "/beatbuddy" command, improving message posting through "slackApi". Error handling for Slack API responses is also upgraded, contributing to smoother interaction and compatibility with Netlify serverless functions.

--- src/slack.ts
+++ src/slack.ts
@@ -1,6 +1,41 @@
 import type { Handler } from "@netlify/functions";
 
-export const handler: Handler = async () => {
+import { parse } from "querystring";
+import { slackApi } from "./utils/slack";
+
+async function handleSlashCommand(payload: SlackSlashCommandPayload) {
+  switch (payload.command) {
+    case "/beatbuddy":
+      const response = await slackApi("chat.postMessage", {
+        channel: payload.channel_id,
+        text: "Things are happening!",
+      });
+
+      if (!response.ok) {
+        console.log(response);
+      }
+
+      break;
+
+    default:
+      return {
+        statusCode: 200,
+        body: `Command ${payload.command} not supported`,
+      };
+  }
+
+  return {
+    statusCode: 200,
+    body: "",
+  };
+}
+
+export const handler: Handler = async (event) => {
+  const body = parse(event.body ?? "") as SlackPayload;
+  if (body.command) {
+    return handleSlashCommand(body as SlackSlashCommandPayload);
+  }
+
   return {
     statusCode: 200,
     body: "Hello World",
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,27 @@
 type SlackApiEndpoint = "chat.postMessage" | "views.open";
 
 type SlackApiRequestBody = {};
+
+type SlackSlashCommandPayload = {
+  token: string;
+  team_id: string;
+  team_domain: string;
+  channel_id: string;
+  channel_name: string;
+  user_id: string;
+  user_name: string;
+  command: string;
+  text: string;
+  api_app_id: string;
+  is_enterprise_install: boolean;
+  response_url: string;
+  trigger_id: string;
+  payload: never;
+};
+
+type SlackInteractivityPayload = {
+  payload: string;
+  command: never;
+};
+
+type SlackPayload = SlackSlashCommandPayload | SlackInteractivityPayload;
