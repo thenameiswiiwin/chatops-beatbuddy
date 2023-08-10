baa000d572545ccec0d3f7cfc3fbf23cba0214f1
thenameiswiiwin <huyn.nguyen95@gmail.com>
2023-08-10 12:08:42 -0400

    verifying the authenticity of Slack requests. The code checks the provided signature against a calculated hash that includes the request timestamp and body. Requests with a timestamp difference of over 300 seconds are rejected, enhancing security for interactions with Slack APIs.

--- src/utils/slack.ts
+++ src/utils/slack.ts
@@ -1,3 +1,7 @@
+import type { HandlerEvent } from "@netlify/functions";
+
+import { createHmac } from "crypto";
+
 export function slackApi(
   endpoint: SlackApiEndpoint,
   body: SlackApiRequestBody
@@ -10,4 +14,21 @@ export function slackApi(
     },
     body: JSON.stringify(body),
   }).then((res) => res.json());
+}
+
+export function verifySlackRequest(request: HandlerEvent) {
+  const secret = process.env.SLACK_SIGNING_SECRET!;
+  const signature = request.headers["x-slack-signature"];
+  const timestmap = Number(request.headers["x-slack-request-timestamp"]);
+  const now = Math.floor(Date.now() / 1000);
+
+  if (Math.abs(now - timestmap) > 300) {
+    return false;
+  }
+
+  const hash = createHmac("sha256", secret)
+    .update(`v0:${timestmap}:${request.body}`)
+    .digest("hex");
+
+  return `v0=${hash}` === signature;
 }
