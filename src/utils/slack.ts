257729bac4ce5c27cf336c59186cb57e231ee868
thenameiswiiwin <huyn.nguyen95@gmail.com>
2023-08-11 08:25:55 -0400

    adds utility functions for generating different Slack block types: section, input, and select. These functions simplify the creation of formatted Slack blocks by handling various configurations and content.

--- src/utils/slack.ts
+++ src/utils/slack.ts
@@ -32,3 +32,78 @@ export function verifySlackRequest(request: HandlerEvent) {
 
   return `v0=${hash}` === signature;
 }
+
+export const blocks = {
+  section: ({ text }: SectionBlockArgs): SlackBlockSection => {
+    return {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text,
+      },
+    };
+  },
+  input: ({
+    id,
+    label,
+    placeholder,
+    initial_value = "",
+    hint = "",
+  }: InputBlockArgs): SlackBlockInput => {
+    return {
+      block_id: `${id}_block`,
+      type: "input",
+      label: {
+        type: "plain_text",
+        text: label,
+      },
+      element: {
+        action_id: id,
+        type: "plain_text_input",
+        placeholder: {
+          type: "plain_text",
+          text: placeholder,
+        },
+        initial_value,
+      },
+      hint: {
+        type: "plain_text",
+        text: hint,
+      },
+    };
+  },
+  select: ({
+    id,
+    label,
+    placeholder,
+    options,
+  }: SelectBlockArgs): SlackBlockInput => {
+    return {
+      block_id: `${id}_block`,
+      type: "input",
+      label: {
+        type: "plain_text",
+        text: label,
+      },
+      element: {
+        action_id: id,
+        type: "static_select",
+        placeholder: {
+          type: "plain_text",
+          text: placeholder,
+          emoji: true,
+        },
+        options: options.map(({ label, value }) => {
+          return {
+            text: {
+              type: "plain_text",
+              text: label,
+              emoji: true,
+            },
+            value,
+          };
+        }),
+      },
+    };
+  },
+};
--- types/index.d.ts
+++ types/index.d.ts
@@ -25,3 +25,74 @@ type SlackInteractivityPayload = {
 };
 
 type SlackPayload = SlackSlashCommandPayload | SlackInteractivityPayload;
+
+type SectionBlockArgs = {
+  text: string;
+};
+
+type BlockArgs = {
+  id: string;
+  label: string;
+  placeholder: string;
+};
+
+type InputBlockArgs = {
+  initial_value?: string;
+  hint?: string;
+} & BlockArgs;
+
+type SelectBlockArgs = {
+  options: {
+    label: string;
+    value: string;
+  }[];
+} & BlockArgs;
+
+type SlackBlockSection = {
+  type: "section";
+  text: {
+    type: "plain_text" | "mrkdwn";
+    text: string;
+    verbatim?: boolean;
+  };
+};
+
+type SlackBlockInput = {
+  type: "input";
+  block_id: string;
+  label: {
+    type: "plain_text";
+    text: string;
+    emoji?: boolean;
+  };
+  hint?: {
+    type: "plain_text";
+    text: string;
+    emoji?: boolean;
+  };
+  optional?: boolean;
+  dispatch_action?: boolean;
+  element: {
+    type: string;
+    action_id: string;
+    placeholder?: {
+      type: string;
+      text: string;
+      emoji?: boolean;
+    };
+    options?: {
+      text: {
+        type: "plain_text";
+        text: string;
+        emoji?: boolean;
+      };
+      value: string;
+    }[];
+    initial_value?: string;
+    dispatch_action_config?: {
+      trigger_actions_on: string[];
+    };
+  };
+};
+
+type SlackBlock = SlackBlockSection | SlackBlockInput;
