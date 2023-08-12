37c98a2132f925be912fba3a54102102fb50a885
thenameiswiiwin <huyn.nguyen95@gmail.com>
2023-08-12 10:07:59 -0500

    Music sharing modal and input setup
    Slack modal for music sharing within the BeatBuddy environment. The modal prompts users to input their favorite song and artist, along with selecting the song's genre. This update enhances the user experience by allowing easy sharing of musical preferences and fostering collaboration among users.

--- src/slack.ts
+++ src/slack.ts
@@ -1,18 +1,58 @@
 import type { Handler } from "@netlify/functions";
 
 import { parse } from "querystring";
-import { slackApi, verifySlackRequest } from "./utils/slack";
+import { blocks, modal, slackApi, verifySlackRequest } from "./utils/slack";
 
 async function handleSlashCommand(payload: SlackSlashCommandPayload) {
   switch (payload.command) {
     case "/beatbuddy":
-      const response = await slackApi("chat.postMessage", {
-        channel: payload.channel_id,
-        text: "Things are happening!",
-      });
+      const res = await slackApi(
+        "views.open",
+        modal({
+          id: "beatbuddy-modal",
+          title: "Start sharing!",
+          trigger_id: payload.trigger_id,
+          blocks: [
+            blocks.section({
+              text: "Welcome to BeatBuddy!\n\nGet ready to groove with your favorite tunes. Share the music that gets you in the zone and lets your creativity flow. Whether it's your all-time favorite song or the artist that brings out your musical spirit, let's create some awesome rhythms together!",
+            }),
+            blocks.input({
+              id: "song-input",
+              label:
+                "Deposite your favorite song and artist you like to jam to here.",
+              placeholder: "Example: Essence - Wizkid ft. Tems",
+              initial_value: payload.text ?? "",
+              hint: "Provide the name of the song you like to jam to!",
+            }),
+            blocks.select({
+              id: "song-genre",
+              label: "Select the song's genre",
+              placeholder: "Choose a genre",
+              options: [
+                { label: "Pop", value: "pop" },
+                { label: "Hip-Hop", value: "hip-hop" },
+                { label: "R&B", value: "r&b" },
+                { label: "Latin", value: "latin" },
+                { label: "Rock", value: "rock" },
+                { label: "Electronic", value: "electronic" },
+                { label: "Country", value: "country" },
+                { label: "Jazz", value: "jazz" },
+                { label: "Blues", value: "blues" },
+                { label: "Reggae", value: "reggae" },
+                { label: "Funk", value: "funk" },
+                { label: "Classical", value: "classical" },
+                { label: "Indie", value: "indie" },
+                { label: "Metal", value: "metal" },
+                { label: "Alternative", value: "alternative" },
+                { label: "Dance", value: "dance" },
+              ],
+            }),
+          ],
+        })
+      );
 
-      if (!response.ok) {
-        console.log(response);
+      if (!res.ok) {
+        console.log(res);
       }
 
       break;
@@ -48,6 +88,6 @@ export const handler: Handler = async (event) => {
 
   return {
     statusCode: 200,
-    body: "Hello World",
+    body: "Request successfully processed.",
   };
 };
